0247ae82af212709f7610ed32ec50fda046701ff
Scott Hahn <scotthahn2210@gmail.com>
2023-10-23 01:50:16 +0200

    moved server.ts folder

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,7 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import router from '../routes/weatherRoutes';
+import router from './routes/weatherRoutes';
 import serverless from 'serverless-http';
 
 dotenv.config();
